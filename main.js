c3d86c839231cd867ff9a571be49bfd5f79b1b72
Felipe Souza <felipesouza0500@gmail.com>
2025-09-03 21:27:00 -0300

    add set()

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
 import interfaceDoUser from "./interface.js"
 import api from "./backend/api.js";
 
+const pensamentosSet = new Set()
+
+async function adicionarChaveAoPensamento() {
+    try {
+        const pensamentos = await api.buscar()
+        pensamentos.forEach(pensamento => {
+            const chavePensamento = `${pensamento.conteudo.trim().toLowerCase()}-${pensamento.autoria.trim().toLowerCase()}`
+            pensamentosSet.add(chavePensamento)
+        });
+    } catch (error) {
+        alert("Erro ao adicionar chave ao pensamento!")        
+    }
+}
+
 function removerEspaços(string){
     return string.replaceAll(/\s+/g, '')
 }
 
 const regexConteudo = /^[A-Za-z\s]{10,}$/
-
 function validarConteudo(conteudo) {
     return regexConteudo.test(conteudo)
 }
 
 const regexAutoria = /^[a-zA-Z]{3,15}$/;
-
 function validarAutoria(autoria) {
   return regexAutoria.test(autoria);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     interfaceDoUser.renderizar()
+    adicionarChaveAoPensamento()
 
     const formulario = document.getElementById("pensamento-form");
     formulario.addEventListener("submit", envioDoFormulario)
 
     const inputBusca = document.getElementById("campo-busca")
     inputBusca.addEventListener("input", manipularBusca)
+
+    const cancelar = document.getElementById("botao-cancelar");
+    cancelar.addEventListener("click", interfaceDoUser.limparFormulario);
 
 })
 
@@ -53,6 +69,13 @@ async function envioDoFormulario(e) {
         alert("DATA: Não é permitido o cadastro da datas futuras!")
     }
 
+    const chaveNovoPensamento = `${conteudo.trim().toLowerCase()}-${autoria.trim().toLowerCase()}`
+
+    if(pensamentosSet.has(chaveNovoPensamento)){
+        alert('Esse pensamento já existe')
+        return
+    }
+
     try {
         if (id){
             await api.editar({ id, favorito, conteudo, autoria, data })
@@ -65,8 +88,7 @@ async function envioDoFormulario(e) {
     }
 }
 
-const cancelar = document.getElementById("botao-cancelar");
-cancelar.addEventListener("click", interfaceDoUser.limparFormulario);
+
 
 async function manipularBusca() {
     const termoBusca = document.getElementById("campo-busca").value;
